10d2272b56a8faea0cf27dbe4b3014f3dd66a013
MRDaSim <danialbrecht9@gmail.com>
2022-11-24 22:41:18 -0300

    ese es el commit pricipal de qumaxEnlace

---
+++ src/main/java/com/qumaxenlace/qumaxEnlace/QumaxEnlaceApplication.java
@@ -0,0 +1,13 @@
+package com.qumaxenlace.qumaxEnlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QumaxEnlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QumaxEnlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qumaxenlace/qumaxEnlace/QumaxEnlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qumaxenlace.qumaxEnlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QumaxEnlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
